680d49368e589b493731cfe5d1a419b200f353a0
Lakshit Bhutani <lakshitbhutani11@gmail.com>
2015-12-29 23:40:48 +0530

    added login page

---
+++ login.php
@@ -0,0 +1,50 @@
+<html>
+<head>
+	<title>Worldlink</title>
+	<style type="text/css">
+<!-- 
+A{text-decoration:none} 
+-->
+</style>
+</head>
+<body bgcolor="black" text="white" alink="white" vlink="white" link="white">
+<div align="center"><font size="7">Welcome to Worldlink</font></div>
+<br>
+<h3>
+<form action="authenticate.php" method="POST">
+	<hr>
+	<img src="world_link2.jpg" alt="Worldlink" width="950" height="450" align="left">
+	<br><br><br> 
+	<div align="right">
+	Username<br>
+	 <input type ="text" name="username" placeholder="Username">
+    <br><br>
+    Password <br>
+    <input type="password" name="password" placeholder="Password">
+    <br><br><br><br>
+    <input type="submit" name="login" value="LOGIN"> &nbsp;&nbsp;&nbsp;
+    <input type="reset"  value="Reset">
+    </div>
+</form>
+
+<!--<?php
+
+if (null!=$_GET['notify'])
+ {
+	echo $_GET['notify'];
+ }	
+?>
+-->
+</h3>
+<br><br><br><br><br>
+<body>
+	<h3>
+	<div align="right">
+    <a href="register.php">Register here for free!</a>
+    </h3>
+    </div>
+    <br><br><br><br>
+    
+</body>
+</body>
+</html>
